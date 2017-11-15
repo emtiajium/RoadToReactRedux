d8f516dd3d79c9766ede5ecfc550fc4d02c9f733
Emtiaj <271emtiaj@gmail.com>
2017-11-15 16:08:05 +0600

    main component to be passed in index.html

--- client/src/components/main.component.js
+++ client/src/components/main.component.js
@@ -1,3 +1,28 @@
 /**
  * Created by emtiaj on 11/15/17.
  */
+
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import SidebarContent from './sidebar.component';
+import Routes from '../routes';
+
+class MainComponent extends Component {
+  render() {
+    let element = (
+      <Router>
+        <div className="row">
+          <div className="col-sm-3">
+            <SidebarContent/>
+          </div>
+          <div className="col-sm-9">
+            <Routes/>
+          </div>
+        </div>
+      </Router>
+    );
+    return element;
+  }
+}
+
+export default MainComponent;
