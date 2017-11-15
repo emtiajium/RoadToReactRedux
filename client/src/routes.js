c13dee27b29280c975fb3152db1770cdf7cef5f7
Emtiaj <271emtiaj@gmail.com>
2017-11-15 16:09:38 +0600

    removed Router as it is in main.component

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,19 +3,17 @@
  */
 
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import UserComponent from './components/user.component';
 import UserInfoComponent from './components/userInfo.component';
 
 class AllRoutes extends Component {
   render () {
     let element = (
-      <Router>
-        <div>
-          <Route path='/user' component={UserComponent} />
-          <Route path='/user_info' component={UserInfoComponent} />
-        </div>
-      </Router>
+      <div>
+        <Route path='/user' component={UserComponent} />
+        <Route path='/user_info' component={UserInfoComponent} />
+      </div>
     );
     return element;
   }
