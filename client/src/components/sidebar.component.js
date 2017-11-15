c83f4266363dcf0a2cc9689ba370050013b2ee31
Emtiaj <271emtiaj@gmail.com>
2017-11-15 13:45:19 +0600

    sidebar, menu list

--- client/src/components/sidebar.component.js
+++ client/src/components/sidebar.component.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, NavLink} from 'react-router-dom';
 
-let RouteContent = (
-  <Router>
-    <ul className="nav">
-      <li>
-        <NavLink to='/user'>User</NavLink>
-      </li>
-      <li>
-        <NavLink to='/user_info'>User Info</NavLink>
-      </li>
-    </ul>
-  </Router>
-);
+class SidebarContent extends Component {
+  render() {
+    /**
+     * here importing `Router` is necessary
+     * otherwise `NavLink` won't work
+     * error message: You should not use <Route> or withRouter() outside a <Router>
+     */
+    let element = (
+      <Router>
+        <ul className="nav">
+          <li>
+            <NavLink to='/user'>User</NavLink>
+          </li>
+          <li>
+            <NavLink to='/user_info'>User Info</NavLink>
+          </li>
+        </ul>
+      </Router>
+    );
+    return element;
+  }
+}
 
-export default RouteContent;
+export default SidebarContent;
