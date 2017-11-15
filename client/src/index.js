58fe5a69dba34663f142b2a3be27c404b180af7c
Emtiaj <271emtiaj@gmail.com>
2017-11-15 13:46:15 +0600

    render two components

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import './style/importStyle';
+import SidebarContent from './components/sidebar.component';
 import Routes from './routes';
 import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
 let store = configureStore();
+
+render(
+  <SidebarContent/>,
+  document.getElementById('sidebar-content')
+);
 
 render(
   <Provider store={store}>
