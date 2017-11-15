21852aa5fbe803ad2cc5eb4653c92c852b822ca0
Emtiaj <271emtiaj@gmail.com>
2017-11-15 13:45:53 +0600

    as separation is in index.html

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,7 +3,7 @@
  */
 
 import React, {Component} from 'react';
-import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import UserComponent from './components/user.component';
 import UserInfoComponent from './components/userInfo.component';
 
@@ -11,21 +11,9 @@ class AllRoutes extends Component {
   render () {
     let element = (
       <Router>
-        <div className="row">
-          <div className="col-sm-3">
-            <ul className="nav">
-              <li>
-                <NavLink to='/user'>User</NavLink>
-              </li>
-              <li>
-                <NavLink to='/user_info'>User Info</NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className="col-sm-9">
-            <Route path='/user' component={UserComponent} />
-            <Route path='/user_info' component={UserInfoComponent} />
-          </div>
+        <div>
+          <Route path='/user' component={UserComponent} />
+          <Route path='/user_info' component={UserInfoComponent} />
         </div>
       </Router>
     );
